2bbdf199ace4598149311dbb5eaa3a342f33a349
basia <b.rurkowska@gmail.coM>
2015-05-17 21:28:07 +0200

    aplikacje

---
+++ KAAALKULATOR/KAAALKULATOR/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace KAAALKULATOR
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void wynik_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+    
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void podziel_Click(object sender, RoutedEventArgs e)
+        {
+            int num = Convert.ToInt32(this.liczba1.Text);
+            double a = ((double)Convert.ToInt32(this.liczba1.Text)) / ((double)Convert.ToInt32(this.liczba2.Text));
+
+            {
+                this.wynik.Text = a.ToString();
+            }
+        }
+
+        private void odejmij_Click(object sender, RoutedEventArgs e)
+        {
+            int num = Convert.ToInt32(this.liczba1.Text);
+            double a = ((double)Convert.ToInt32(this.liczba1.Text)) - ((double)Convert.ToInt32(this.liczba2.Text));
+
+            {
+                this.wynik.Text = a.ToString();
+            }
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            int num = Convert.ToInt32(this.liczba1.Text);
+            double a = ((double)Convert.ToInt32(this.liczba1.Text)) + ((double)Convert.ToInt32(this.liczba2.Text));
+
+            {
+                this.wynik.Text =  a.ToString();
+            }
+        }
+
+        private void mnoz_Click(object sender, RoutedEventArgs e)
+        {
+            int num = Convert.ToInt32(this.liczba1.Text);
+            double a = ((double)Convert.ToInt32(this.liczba1.Text)) * ((double)Convert.ToInt32(this.liczba2.Text));
+
+            {
+                this.wynik.Text = a.ToString();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        }
+        }
+        
+    
---
+++ bmi/bmi/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace bmi
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            int num = Convert.ToInt32(this.Wzrost.Text);
+            double a = ((double)Convert.ToInt32(this.Waga.Text)) / (Math.Pow((double)num, 2.0) / 10000.0);
+            a = Math.Round(a);
+            if (a < 18.5)
+            {
+                this.box.Text = "Twoje BMI wynosi: " + a.ToString() + " - niedowaga";
+            }
+            else if (a >= 18.5)
+            {
+                this.box.Text = "Twoje BMI wynosi: " + a.ToString() + " - waga prawidłowa";
+            }
+            else if (a >= 25.0)
+            {
+                this.box.Text = "Twoje BMI wynosi: " + a.ToString() + " - nadwaga";
+            }
+            else
+            {
+                this.box.Text = "Twoje BMI wynosi: " + a.ToString() + " - otyłość";
+            }
+        }
+    }
+}

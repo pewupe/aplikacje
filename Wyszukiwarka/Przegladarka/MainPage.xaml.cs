3f61349876e5e33a65cb73b3c0af08a101b3eb27
basia <b.rurkowska@gmail.coM>
2015-05-18 23:53:07 +0200

    dsfd

---
+++ Wyszukiwarka/Przegladarka/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+
+namespace Przegladarka
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserTask przeglad = new WebBrowserTask();
+            przeglad.Uri = new Uri("http://google.pl", UriKind.Absolute);
+            przeglad.Show();
+        }
+    }
+}
---
+++ cooostam/cooostam/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace cooostam
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (checkBox1.IsChecked == true && checkBox2.IsChecked == true)
+            {
+                MessageBox.Show("Brawo");
+            }
+
+
+            if (checkBox1.IsChecked == false && checkBox2.IsChecked == false)
+            {
+                MessageBox.Show("Do roboty!");
+            }
+            if (checkBox1.IsChecked == true && checkBox2.IsChecked == false)
+            {
+                MessageBox.Show("Wyprowadź psa!");
+            }
+            if (checkBox1.IsChecked == false && checkBox2.IsChecked == true)
+            {
+                MessageBox.Show("Zrób sprawko!");
+            }
+            }
+
+
+        }
+
+    
+}
+
+            
+   
+
+
+                
+
+
+
+     
+
